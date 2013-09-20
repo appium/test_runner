9c3398d20803824a372a260e8166fff8565c18b3
bootstraponline <code@bootstraponline.com>
2013-09-20 15:43:28 -0400

    Release 0.9.23

--- lib/test_runner/version.rb
+++ lib/test_runner/version.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 module TestRunner
-  VERSION = '0.9.22' unless defined? ::TestRunner::VERSION
-  DATE = '2013-09-19' unless defined? ::TestRunner::DATE
+  VERSION = '0.9.23' unless defined? ::TestRunner::VERSION
+  DATE = '2013-09-20' unless defined? ::TestRunner::DATE
 end
